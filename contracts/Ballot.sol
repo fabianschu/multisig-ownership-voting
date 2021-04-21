dcf1b21dca90a8d5ba23d5b69ebbbfad441eefee
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2021-04-21 23:16:28 +0200

    Revert "adds balancer v2"
    This reverts commit a8839d97e03bf37aae6b9121e3170c093aabe172.

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -3,23 +3,16 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "hardhat/console.sol";
 import "./gnosis/base/OwnerManager.sol";
-// import "./balancer/BPool.sol"
 
 contract Ballot is OwnerManager {
-    // BPool bPool;
     // TODO
 
-    // tie ballot to balancer poo
-
-    function setupBalancerPool(address _balancerPool) internal {
-        // bPool = BPool(_balancerPool);
-        // console.log("I SET THE BALANCER POOL :)")
-    }
+    // tie ballot to balancer pool
 
     // add proposal logic:
     // if proposal to add owner is accepted addOwnerWithThreshold is called
     // if proposal to remove owner is accepted removeOwner is called
-
+    
     function addOwner(address _newOwner, uint _newThreshold) public {
         addOwnerWithThreshold(_newOwner, _newThreshold);
     }
