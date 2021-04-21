b1e6c2ed8bdc33e620a1152af022d08b508f2212
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2021-04-21 20:44:21 +0200

    set up BPool, joining possible

--- test/own/Pool.spec.ts
+++ test/own/Pool.spec.ts
@@ -2,11 +2,13 @@ import { expect } from "chai";
 import { deployments, waffle, ethers } from "hardhat";
 import { getSafeWithOwners } from "../gnosis/utils/setup";
 import * as token from "../../build/artifacts/contracts/balancer/test/TToken.sol/TToken.json";
-
-const [alice, bob, carlos, dido, fred] = waffle.provider.getWallets();
+import * as bpool from "../../build/artifacts/contracts/balancer/BPool.sol/BPool.json";
 
 const setupTokens = async () => {
-  // mint tokens to alice
+  // const [alice, bob, carlos, dido, fred] = waffle.provider.getWallets();
+  const [alice, bob, carlos] = await ethers.getSigners();
+  const MAX = ethers.constants.MaxUint256;
+  // set up token contracts
   const wethInstance = await waffle.deployContract(alice, token, [
     "Wrapped Ether",
     "WETH",
@@ -17,12 +19,52 @@ const setupTokens = async () => {
     "DAI",
     18,
   ]);
-  console.log("deployment of tokens successful");
-  const bPoolInstance = await ethers.getContract("BPool");
-  console.log("Minting 50 Ether to Alice");
+  const bPoolinstance = await waffle.deployContract(alice, bpool, []);
+
+  // mint to users
   await wethInstance.mint(alice.address, ethers.utils.parseEther("50"));
-  console.log(await wethInstance.balanceOf(alice.address));
-  return { wethInstance, daiInstance };
+  await daiInstance.mint(alice.address, ethers.utils.parseEther("10000"));
+  await wethInstance.mint(bob.address, ethers.utils.parseEther("25"));
+  await daiInstance.mint(bob.address, ethers.utils.parseEther("5000"));
+  await wethInstance.mint(carlos.address, ethers.utils.parseEther("100"));
+  await daiInstance.mint(carlos.address, ethers.utils.parseEther("200000"));
+
+  // approve tokens
+  await wethInstance.approve(bPoolinstance.address, MAX);
+  await daiInstance.approve(bPoolinstance.address, MAX);
+
+  // bind BPool with DAI and WETH
+  await bPoolinstance.bind(
+    wethInstance.address,
+    ethers.utils.parseEther("50"),
+    ethers.utils.parseEther("5")
+  );
+  await bPoolinstance.bind(
+    daiInstance.address,
+    ethers.utils.parseEther("10000"),
+    ethers.utils.parseEther("5")
+  );
+
+  // set pool public
+  await bPoolinstance.setPublicSwap(true);
+
+  // finalize pool
+  await bPoolinstance.finalize();
+
+  // other users authorize pool to transfer tokens
+  await wethInstance.connect(bob).approve(bPoolinstance.address, MAX);
+  await daiInstance.connect(bob).approve(bPoolinstance.address, MAX);
+  await wethInstance.connect(carlos).approve(bPoolinstance.address, MAX);
+  await daiInstance.connect(carlos).approve(bPoolinstance.address, MAX);
+
+  console.log(await bPoolinstance.balanceOf(bob.address));
+  // bob joins pool
+  await bPoolinstance
+    .connect(bob)
+    .joinPool(ethers.utils.parseEther("5"), [MAX, MAX]);
+
+  console.log(await bPoolinstance.balanceOf(bob.address));
+  return { wethInstance, daiInstance, bPoolinstance };
 };
 
 describe("BPool", async () => {
@@ -32,7 +74,8 @@ describe("BPool", async () => {
 
   describe("#addOwner", () => {
     it("should add a new owner", async () => {
-      const { wethInstance, daiInstance } = await setupTokens();
+      await setupTokens();
+      // const { wethInstance, daiInstance } = await setupTokens();
     });
   });
 });
